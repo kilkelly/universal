cbb05349e3f545b917cbe811237047bcff63b2ed
Frank Kilkelly <frank.kilkelly@gmail.com>
2016-02-05 14:27:53 +0000

    Components to show example of calling this.props.children from a grouping component to display any number of child components

---
+++ shared/components/pure/IAmAChild.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+// STATELESS FUNCTIONAL COMPONENT - See section in README.md
+const IAmAChild = props => {
+	return(
+		<span>I am a child.</span>
+	)	
+}
+
+export default IAmAChild
---
+++ shared/components/pure/OneOrManyChildGrouping.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+// STATELESS FUNCTIONAL COMPONENT - See section in README.md
+const OneOrManyChildGrouping = props => {
+	return(
+		<div>{props.children}</div>
+	)	
+}
+
+export default OneOrManyChildGrouping
